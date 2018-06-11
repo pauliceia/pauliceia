49d96f2ad6c397a37e595a025cbef5c76b18c899
Beto Noronha <beto_noronha@live.com>
2018-06-11 14:10:54 -0300

    base url in production

--- src/middleware/ApiVGI.js
+++ src/middleware/ApiVGI.js
@@ -2,14 +2,17 @@ import axios from 'axios'
 import store from '@/store'
 
 export default () => {
+  let baseURL = 'http://localhost:8888/'
+  if(NODE_ENV == "production") baseURL = 'x'
+  
   return store.state.auth.token ? 
     axios.create({
-      baseURL: `http://localhost:8888/`,
+      baseURL,
       headers: {
         'Authorization': store.state.auth.token
       }
     }) :
     axios.create({
-      baseURL: `http://localhost:8888/`
+      baseURL
     })
 }
