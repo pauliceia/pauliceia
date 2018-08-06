519a04ac899c38a58bed7fd4194730828d14e612
Denis Taveira <denis.taveira@hotmail.com>
2018-08-06 03:19:25 -0300

    edit boxNotifications

--- src/views/components/map/BoxNotifications.vue
+++ src/views/components/map/BoxNotifications.vue
@@ -1,65 +1,69 @@
 <template>
     <div class="box-info" v-show="boxNotifications">
-        <header class="header">
-            <h1>NOTIFICATIONS</h1>
-            <button class="btn" @click="closeBox()">
-                <md-icon>close</md-icon>
-            </button>
-        </header>
-        <div class="body">
-            <el-tabs v-model="activeName" @tab-click="handleClick">
-                <el-tab-pane label="GENERAL" name="first">
-                    <div class="nofitication">
-                        <div v-for="test in 8" :key="test">
-                            <div class="notification-box">
+      <header class="header">
+        <h1>NOTIFICATIONS</h1>
+        <button class="btn" @click="closeBox()">
+          <md-icon>close</md-icon>
+        </button>
+      </header>
+      <div class="body">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="GENERAL" name="first">
+            <div class="nofitication">
+              <div v-for="n in notif">
+                <div class="notification-box">
 
-                                <div style="display: flex; align-items: center;">
-                                    <div class="photo">B</div>
-                                    <div class="credentials">
-                                        <p class="author">Beto Noronha</p>
-                                        <p class="date">05/02/2018</p>
-                                    </div>
-                                </div>
-                                
-                                <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                                <p class="comments">see more</p>
-                            </div>
-                        </div>
+                  <div style="display: flex; align-items: center;">
+                    <div class="photo">B</div>
+                    <div class="credentials">
+                      <p class="author">{{n.name}}</p>
+                      <p class="date">{{n.date}}</p>
                     </div>
-                </el-tab-pane>
-                
-                <el-tab-pane label="FOLLOWING" name="second">
-                    <div v-for="test in 8" :key="test">
-                            <div class="notification-box">
+                  </div>
 
-                                <div style="display: flex; align-items: center;">
-                                    <div class="photo">C</div>
-                                    <div class="credentials">
-                                        <p class="author">Carlos Noronha</p>
-                                        <p class="date">05/02/2018</p>
-                                    </div>
-                                </div>
-                                
-                                <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                                <p class="comments">see more</p>
-                            </div>
-                        </div>
-                </el-tab-pane>
-            </el-tabs>
-        </div>
+                  <p class="content">{{n.description}}</p>
+                  <p class="comments">Answer</p>
+                </div>
+              </div>
+            </div>
+          </el-tab-pane>
+
+          <el-tab-pane label="FOLLOWING" name="second">
+            <div v-for="test in 8" :key="test">
+              <div class="notification-box">
+
+                <div style="display: flex; align-items: center;">
+                  <div class="photo">C</div>
+                  <div class="credentials">
+                    <p class="author">Carlos Noronha</p>
+                    <p class="date">05/02/2018</p>
+                  </div>
+                </div>
+
+                <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p class="comments">see more</p>
+              </div>
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+  import Api from '@/middleware/ApiVGI'
+  import { mapState } from 'vuex'
 
 export default {
     computed: {
-      ...mapState('map', ['boxNotifications'])
+      ...mapState('map', ['boxNotifications']),
+      ...mapState('auth', ['isUserLoggedIn', 'user'])
     },
     data() {
       return {
-        activeName: 'first'
+        activeName: 'first',
+        notifications: [],
+        notif: [],
       };
     },
     methods: {
@@ -69,6 +73,24 @@ export default {
         handleClick(tab, event) {
             // console.log(tab, event);
         }
+    },
+    beforeCreate() {
+      const vm = this
+      Api().get('/api/notification/').then(function (response) {
+        //Api().get('/api/notification/?notification_id=' + vm.user.user_id).then(function (response) {
+        Api().get('/api/notification/').then(function (response) {
+          response.data.features.filter(e => {
+            vm.notifications.push(e.properties)
+
+            Api().get('/api/user/?user_id='+e.properties.user_id_creator).then(function (response) {
+              vm.notif.push({'description': e.properties.description, 'name':response.data.features[0].properties.name, 'date': e.properties.created_at})
+              //console.log(response.data.features[0].properties.name)
+            })
+
+          })
+          //console.log(vm.notifications)
+        })
+      })
     }
 }
 </script>
--- src/views/pages/dashboard/home.vue
+++ src/views/pages/dashboard/home.vue
@@ -7,12 +7,47 @@
             Notifications
           </div>
           <div class="card-body">
-            <p class="card-text" v-for="n in notifications">
-              <a href="#">{{ n.user_id_creator }}</a> commented
-              <a href="#">{{ n.name }}</a> on
-              <a href="#">{{ n.layer_id }}</a> at
-              <a href="#">{{ n.created_at }}</a>
-            </p>
+            <div class="body">
+              <el-tabs v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="GENERAL" name="first">
+                  <div class="nofitication">
+                    <div v-for="n in notif">
+                      <div class="notification-box">
+
+                        <div style="display: flex; align-items: center;">
+                          <div class="photo">B</div>
+                          <div class="credentials">
+                            <p class="author">{{n.name}}</p>
+                            <p class="date">{{n.date}}</p>
+                          </div>
+                        </div>
+
+                        <p class="content">{{n.description}}</p>
+                        <p class="comments">Answer</p>
+                      </div>
+                    </div>
+                  </div>
+                </el-tab-pane>
+
+                <el-tab-pane label="FOLLOWING" name="second">
+                  <div v-for="test in 8" :key="test">
+                    <div class="notification-box">
+
+                      <div style="display: flex; align-items: center;">
+                        <div class="photo">C</div>
+                        <div class="credentials">
+                          <p class="author">Carlos Noronha</p>
+                          <p class="date">05/02/2018</p>
+                        </div>
+                      </div>
+
+                      <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                      <p class="comments">see more</p>
+                    </div>
+                  </div>
+                </el-tab-pane>
+              </el-tabs>
+            </div>
           </div>
         </div>
       </div>
@@ -23,13 +58,13 @@
           </div>
           <div class="card-body">
             <div class="card-text">
-              <div class="row" v-for="layer in layers">
+              <div class="row" v-for="layer in myLayers">
                 <div class="col-sm-8">{{ layer.name }}</div>
                 <div class="col-sm-2">
-                  <button type="button" class="btn btn-outline-primary btn-sm add" @click="editeLayer(layer.id)"><md-icon>create</md-icon></button>
+                  <button type="button" class="btn btn-outline-primary btn-sm add" @click="editeLayer(layer.layer_id)"><md-icon>create</md-icon></button>
                 </div>
                 <div class="col-sm-2">
-                  <button type="button" class="btn btn-outline-danger btn-sm add" @click="deleteLayer(layer.id)"><md-icon>clear</md-icon></button>
+                  <button type="button" class="btn btn-outline-danger btn-sm add" @click="deleteLayer(layer.layer_id)"><md-icon>clear</md-icon></button>
                 </div>
                 <hr>
               </div>
@@ -42,13 +77,13 @@
           </div>
           <div class="card-body">
             <div class="card-text">
-              <div class="row" v-for="layer in layers">
+              <div class="row" v-for="layer in sharedLayers">
                 <div class="col-sm-8">{{ layer.name }}</div>
                 <div class="col-sm-2">
-                  <button type="button" class="btn btn-outline-primary btn-sm add" @click="editeLayer(layer.id)"><md-icon>create</md-icon></button>
+                  <button type="button" class="btn btn-outline-primary btn-sm add" @click="editeLayer(layer.layer_id)"><md-icon>create</md-icon></button>
                 </div>
                 <div class="col-sm-2">
-                  <button type="button" class="btn btn-outline-danger btn-sm add" @click="deleteLayer(layer.id)"><md-icon>clear</md-icon></button>
+                  <button type="button" class="btn btn-outline-danger btn-sm add" @click="deleteLayer(layer.layer_id)"><md-icon>clear</md-icon></button>
                 </div>
                 <hr>
               </div>
@@ -70,37 +105,19 @@
       "p-dash-layout": DashLayout
     },
     computed: {
-      ...mapState('auth', ['isUserLoggedIn', 'user'])
+      ...mapState('auth', ['isUserLoggedIn', 'user']),
+      ...mapState('map', ['boxNotifications']),
     },
     data: function () {
       return {
-        notifications: [
-          {
-            id: 1,
-            name: 'Ano Incorreto',
-            description: 'Ano Incorreto',
-            icon: '',
-            created_at: '23/05/2018 18:45',
-            user_id_creator: 'Rodrigo',
-            layer_id: 'Streets in 1920',
-            theme_id: 1,
-            notification_id_parent: 1
-          },
-          {
-            id: 2,
-            name: 'Localização Errada',
-            description: 'Ano incorreto',
-            icon: '',
-            created_at: '23/05/2018 14:50',
-            user_id_creator: 'Beto',
-            layer_id: 'Hospitals between 1800 to 1950',
-            theme_id: 1,
-            notification_id_parent: 1
-          }
-        ],
+        myLayers: [],
+        sharedLayers: [],
         layers: [],
         users: [],
-        shownNotif: []
+        shownNotif: [],
+        activeName: 'first',
+        notifications: [],
+        notif: [],
       }
     },
     methods: {
@@ -119,17 +136,51 @@
       },
       editeLayer(id){
         this.$router.push({name: 'EditLayer', params: {layer_id: id}})
+      },
+      handleClick(tab, event) {
+        // console.log(tab, event);
       }
     },
     beforeCreate() {
       const vm = this
+
       Api().get('/api/layer').then(function (response) {
         response.data.features.filter(e => {
-          //if(e.properties.user_id_published_by == vm.user.user_id)
           vm.layers.push({name: e.properties.name, id: e.properties.layer_id})
         })
-      })
 
+        Api().get('/api/user_layer/?user_id='+vm.user.user_id).then(function (response) {
+          //console.log(response.data.features)
+          response.data.features.filter(e => {
+            Api().get('/api/layer/?layer_id='+e.properties.layer_id).then(function (response2) {
+              //console.log(response2.data.features[0].properties)
+              if(e.properties.is_the_creator === true){
+                vm.myLayers.push(response2.data.features[0].properties)
+              }
+              else{
+                vm.sharedLayers.push(response2.data.features[0].properties)
+              }
+            })
+          })
+        })
+
+      })
+      Api().get('/api/notification/').then(function (response) {
+        //Api().get('/api/notification/?notification_id=' + vm.user.user_id).then(function (response) {
+        Api().get('/api/notification/').then(function (response) {
+          response.data.features.filter(e => {
+            vm.notifications.push(e.properties)
+
+            Api().get('/api/user/?user_id='+e.properties.user_id_creator).then(function (response) {
+              vm.notif.push({'description': e.properties.description, 'name':response.data.features[0].properties.name, 'date': e.properties.created_at})
+              //console.log(response.data.features[0].properties.name)
+            })
+
+          })
+          //console.log(vm.notifications)
+        })
+      })
+      
       Api().get('/api/user').then(function (response) {
         response.data.features.filter(e => {
           vm.users.push(e.properties)
@@ -141,12 +192,52 @@
 </script>
 
 <style lang="sass" scoped>
-  .add
-    top: 0px
-    display: inline-block
-    border: none
-    padding: 0px
-    margin: 0px
-    position: absolute
+.add
+  top: 0px
+  display: inline-block
+  border: none
+  padding: 0px
+  margin: 0px
+  position: absolute
 
+.body
+  padding: 10px 20px
+  .notification-box
+    margin: 10px
+    background: rgba(#000, 0.1)
+    padding: 20px
+
+    .photo
+      display: inline-block
+      width: 40px
+      padding: 10px
+      text-align: center
+      color: #FFF
+      border-radius: 50%
+      background: #666
+
+    .credentials
+      display: inline-block
+      margin: 0 0 0 10px
+      .author
+        font-weight: 600
+        margin-top: 5px !important
+        font-size: 1.1em
+      .date
+        color: #666
+        font-size: 0.9em
+      p
+        margin: 0 0 5px 0 !important
+
+    .content
+      text-align: justify
+      margin-top: 5px
+
+    .comments
+      width: 100%
+      text-align: right
+      color: #0099ff
+      cursor: pointer
+      margin-top: -10px
+      margin-bottom: 5px
 </style>
