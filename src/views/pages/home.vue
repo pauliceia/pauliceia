a17b71336d0f9447dc9296a6e2c84672c33da895
Rodrigo Mariano <rodmariano13@gmail.com>
2019-06-25 15:53:20 -0300

    Updated How to cite

--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -53,8 +53,16 @@
           <b>{{ $t('home.title4') }}</b>
           <br><br>
           <p>
-            FERREIRA, K. R.; FERLA, L. ; QUEIROZ, G. R. ; VIJAYKUMAR, N. L. ; NORONHA, C. A. ; MARIANO, R. M. ; TAVEIRA, D. ; SANSIGOLO, G. ; GUARNIERI, O. ; ROGERS, T. ; PAGE, M. ; ATIQUE, F. ; MUSA, D. ; SANTOS, J. Y. ; MORAIS, D. S. ; MIYASAKA, C. R. ; ALMEIDA, C. R. ; NASCIMENTO, L. G. M. ; DINIZ, J. A. ; SANTOS, M. C. . A Platform for Collaborative Historical Research based on Volunteered Geographical Information. Journal of Information and Data Management, Vol. 9, No. 3, December 2018, Pages 291–304.
-            <a href="https://periodicos.ufmg.br/index.php/jidm/article/view/426">link</a>
+            FERREIRA, K. R.; FERLA, L.; QUEIROZ, G. R.; VIJAYKUMAR, N. L.; NORONHA, C. A.; 
+            MARIANO, R. M.; TAVEIRA, D.; SANSIGOLO, G.; GUARNIERI, O.; ROGERS, T.; 
+            LESSER, J.; PAGE, M.; ATIQUE, F.; MUSA, D.; SANTOS, J. Y.; MORAIS, D. S.; 
+            MIYASAKA, C. R.; ALMEIDA, C. R.; NASCIMENTO, L. G. M.; DINIZ, J. A.; SANTOS, M. C. 
+            A Platform for Collaborative Historical Research based on Volunteered Geographical 
+            Information. Journal of Information and Data Management, Vol. 9, No. 3, December 2018, 
+            Pages 291–304.
+            <a href="https://periodicos.ufmg.br/index.php/jidm/article/view/426">
+              link
+            </a>
           </p>
         </div>
       </div>
@@ -62,8 +70,14 @@
         <div class="col-sm-9 column-style">
           <br>
           <p>
-            FERREIRA, K. R.; FERLA, L.; QUEIROZ, G. R. de; VIJAYKUMAR, N. L.;NORONHA, C. A.; MARIANO, R. M.; WASSEF, Y.; TAVEIRA, D.; DARDI,I. B.; SANSIGOLO, G.; et al.. Pauliceia 2.0: A Computational Platform for Collaborative Historical Research. In: XVIII Brazilian Symposium on Geoinformatics (GeoInfo 2017), 2017, Salvador. XVIII Brazilian Symposium on Geoinformatics Proceedings, 2017. v. 1. p. 1.
-            <a href="http://mtc-m16c.sid.inpe.br/col/sid.inpe.br/mtc-m16c/2017/12.01.20.22/doc/4ferreira_etal.pdf">link</a>
+            FERREIRA, K. R.; FERLA, L.; QUEIROZ, G. R. de; VIJAYKUMAR, N. L.;NORONHA, C. A.; 
+            MARIANO, R. M.; WASSEF, Y.; TAVEIRA, D.; DARDI,I. B.; SANSIGOLO, G.; et al. 
+            Pauliceia 2.0: A Computational Platform for Collaborative Historical Research. 
+            In: XVIII Brazilian Symposium on Geoinformatics (GeoInfo 2017), 2017, Salvador. 
+            XVIII Brazilian Symposium on Geoinformatics Proceedings, 2017. v. 1. p. 1.
+            <a href="http://mtc-m16c.sid.inpe.br/col/sid.inpe.br/mtc-m16c/2017/12.01.20.22/doc/4ferreira_etal.pdf">
+              link
+            </a>
           </p>
         </div>
       </div>
