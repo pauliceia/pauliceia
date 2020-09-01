bf274e512474134fa93a767f16561386ef9809ed
Rodrigo Mariano <rodmariano13@gmail.com>
2020-09-01 10:41:59 -0300

    toggleLayer method has been refactored; SASS has been updated

--- src/views/components/map/sidebar-layer/LayersItemStatic.vue
+++ src/views/components/map/sidebar-layer/LayersItemStatic.vue
@@ -1,68 +1,69 @@
 <template>
-    <section>
-        <div class="box-item" v-for="layer in layers" :key="layer.title">
-            <span v-bind:class="{ active: layer.status }" @click="toggleLayer(layer)">
-                {{ layer.titleReal }}
-            </span>
-        </div>
-    </section>
+  <section>
+    <div class="box-item" v-for="layer in layers" :key="layer.title">
+      <span v-bind:class="{ active: layer.status }" @click="toggleLayer(layer)">
+        {{ layer.label }}
+      </span>
+    </div>
+  </section>
 </template>
 
 <script>
-  export default {
-    props: {
-      group: Object,
-      color: String
-    },
-
-    data () {
-      return {
-        layers: [
-          {
-            status: true,
-            title: "OSM",
-            titleReal: "Atual (OSM)"
-          }
-        ]
-      }
-    },
-
-    methods: {
-      toggleLayer (layerSelected) {
-        const items = {};
-        for (let i in this.layers) {
-          if (this.layers[i].title === layerSelected.title) {
-            let status = !this.layers[i].status;
-            this.layers[i].status = status;
-            items[this.layers[i].title] = status;
-          }
+export default {
+  props: {
+    group: Object,
+    color: String
+  },
+  data () {
+    return {
+      layers: [
+        {
+          status: true,
+          title: "OSM",
+          label: "Atual (OSM)"
         }
+      ]
+    }
+  },
+  methods: {
+    toggleLayer (selectedLayer) {
+      // change the layer status
+      for (let layer of this.layers) {
+        if (layer.title === selectedLayer.title) {
+          layer.status = !layer.status
+          break
+        }
+      }
 
-        this.group.getLayers().forEach(sublayer => {
-          if (typeof items[sublayer.get('title')] != 'undefined') {
-            console.log(sublayer.get('title'), items[sublayer.get('title')]);
-            sublayer.setVisible(items[sublayer.get('title')])
-          }
-        })
+      // change the visibility based on the new status
+      for (let sublayer of this.group.getLayers().getArray()) {
+        if (sublayer.get('title') === selectedLayer.title) {
+          sublayer.setVisible(selectedLayer.status)
+          break
+        }
       }
     }
   }
+}
 </script>
 
 <style lang="sass" scoped>
-  .box-item
-    margin: 0 0 5px
-    padding: 0
-    display: block
+.box-item
+  margin: 0 0 5px
+  padding: 0
+  display: block
 
-    span
-      padding: 8px 15px
-      font-size: 1.2em
-      border-radius: 5px
-      font-weight: normal
-      display: inline-block
-      background: rgba(#333, .8)
+  span
+    padding: 8px 15px
+    font-size: 1.2em
+    border-radius: 5px
+    font-weight: normal
+    display: inline-block
+    background: rgba(#333, .8)
 
-      &.active
-        background: orangered
+    &.active
+      background: orangered
+
+    &:hover
+      cursor: pointer
 </style>
