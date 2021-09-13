213548233609e578bd2ffea329856151dac3b1a2
Rodrigo Mariano <rodmariano13@gmail.com>
2021-09-13 15:43:15 -0300

    refactor: remove unnecessary function

--- src/middleware/Map.js
+++ src/middleware/Map.js
@@ -15,11 +15,6 @@ export default {
     else return ApiVGI().get(`/api/layer/`)
   },
 
-  getLayers(query) {
-    if(query != null) return ApiVGI().get(`/api/layer/?`+query)
-    else return ApiVGI().get(`/api/layer/`)
-  },
-
   getAttrLayer(query) {
     return ApiVGI().get(`/api/feature_table/?${query}`)
   },
