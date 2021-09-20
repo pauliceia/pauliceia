b14e75126db83fb2cfdc0e40dc763a9f3b438544
Rodrigo Mariano <rodmariano13@gmail.com>
2021-09-20 10:39:55 -0300

    fix: fix methods to add, update and delete comments; add loading component; fix template; fix styles

--- src/views/components/dashboard/ModalNotification.vue
+++ src/views/components/dashboard/ModalNotification.vue
@@ -1,55 +1,79 @@
 <template>
   <div>
-    <el-button type="text" @click="showModal2()">Comments</el-button>
+    <el-button  @click="showModalNotification()">
+      Show comments
+    </el-button>
 
-    <el-dialog title="Notification" :visible.sync="outerVisible" id="myModal" :append-to-body="true" class="mod">
+    <el-dialog title="Comments" :visible.sync="outerVisible" id="myModal"
+               :append-to-body="true" class="mod">
 
       <div class="notification-box">
         <div style="display: flex; align-items: center;">
-          <div class="photo">B</div>
+          <div class="photo">
+            <md-avatar class="md-avatar-icon">
+              <img :src="master_notification.user_picture"/>
+            </md-avatar>
+          </div>
           <div class="credentials">
-            <p class="author">{{notification.name}}</p>
-            <p class="date">{{notification.created_at}}</p>
+            <p class="author">{{ master_notification.user_name }}</p>
+            <p class="date">{{ master_notification.created_at }}</p>
           </div>
         </div>
-        <p class="content">{{notification.description}}</p>
+        <p class="content">{{ master_notification.description }}</p>
       </div>
 
-      <div class="notification-box2" v-if=false>
-        <textarea class="form-control" v-model="txtNotif" id="inputReference" rows="3"></textarea>
-        <br>
-        <div style="right: 30px; position: absolute">
-          <a class="btn btn-primary" @click="addNotif()" style="color: white">Submit</a>
+      <div class="notification-box2">
+        <textarea class="form-control" v-model="description" id="inputReference" rows="3"></textarea>
+        <div style="right: 30px; position: absolute; margin: 10px 0px;">
+          <a class="btn btn-primary" @click="addComment()" style="color: white">
+            Submit
+          </a>
         </div>
-        <p style="left: 0px; display: flex">{{txtReply}}&nbsp;&nbsp;&nbsp;
-          <button type="button" class="btn btn-outline-danger btn-sm add" @click="clearMsg()" v-if="txtReply !== null">
+        <br><br>
+        <!-- <p style="left: 0px; display: flex">
+          {{ txtReply }}
+          &nbsp;&nbsp;&nbsp;
+          <button type="button" v-if="txtReply !== null" @click="cleanMessage()"
+                  class="btn btn-outline-danger btn-sm add">
             <md-icon>clear</md-icon>
           </button>
         </p>
-        <br>
+        <br> -->
       </div>
 
       <div class="nofitication">
-        <div v-for="n in notifG">
-          <div class="notification-box2">
-
+        <div v-for="comment in comments" :key="comment.id">
+          <div class="notification-box2"
+               :style="[comment.is_denunciation ? {'background': 'rgba(255, 255, 0, 0.70)'} : {'background': 'rgba(183, 200, 255, 0.70)'}]"
+          >
             <div style="display: flex; align-items: center;">
-              <div class="photo">B</div>
+              <div class="photo">
+                <md-avatar class="md-avatar-icon">
+                  <img :src="comment.user_picture"/>
+                </md-avatar>
+              </div>
               <div class="credentials">
-                <p class="author">{{n.name}}</p>
-                <p class="date">{{n.date}}</p>
+                <p class="author">{{ comment.user_name }}</p>
+                <p class="date">{{ comment.created_at }}</p>
               </div>
             </div>
 
-            <p class="content">{{n.description}}</p>
+            <p class="content">{{ comment.description }}</p>
+            <p class="content" v-if="comment.is_denunciation">
+              This comment is a denunciation!
+            </p>
             <p class="comments">
-              <button type="button" class="btn btn-outline-primary btn-sm add" @click="replyNot(n)" title="Responder" v-if=false>
+              <!-- <button type="button" @click="replyNotification(comment)"
+                      class="btn btn-outline-primary btn-sm add" title="Responder">
                 <md-icon>replay</md-icon>
               </button>
-              <button type="button" class="btn btn-outline-warning btn-sm add" @click="reportNot(n)" title="Denunciar" v-if=false>
+              <button type="button" @click="reportNotification(comment)"
+                      class="btn btn-outline-warning btn-sm add" title="Denunciar">
                 <md-icon>report</md-icon>
-              </button>
-              <button type="button" class="btn btn-outline-danger btn-sm add" @click="clearNot(n)" v-if="user.user_id === n.user_id_creator || user.is_the_admin" title="Excluir">
+              </button> -->
+              <button type="button" @click="removeComment(comment)"
+                      class="btn btn-outline-danger btn-sm add" title="Excluir"
+                      v-if="canUserRemoveIt(comment)">
                 <md-icon>clear</md-icon>
               </button>
             </p>
@@ -58,8 +82,9 @@
       </div>
 
       <div slot="footer" class="dialog-footer">
-        <el-button @click="outerVisible = false">Close</el-button>
-        <!--<el-button type="primary" @click="innerVisible = true">Close</el-button>-->
+        <el-button @click="closeModalNotification()">
+          Close
+        </el-button>
       </div>
     </el-dialog>
 
@@ -67,180 +92,158 @@
 </template>
 
 <script>
-  import DashLayout from '@/views/layouts/dashboard'
-  import { MessageBox } from 'element-ui';
-  import Api from '@/middleware/ApiVGI'
-  import {mapState} from 'vuex'
+import { mapState } from 'vuex'
 
-  export default {
-    name: "ModalNotification",
-    components: {
-      "modal": {
-        template: '#modal-template'
-      },
-    },
-    computed: {
-      ...mapState('auth', ['isUserLoggedIn', 'user']),
-      ...mapState('map', ['boxNotifications']),
-    },
-    props: ['notification_id'],
-    data: function () {
-      return {
-        activeName: 'first',
-        notification: [],
-        notifG: [],
-        txtNotif: null,
-        keyword_id: null,
-        notification_id_parent: null,
-        is_denunciation: false,
-        layer_id: null,
-        notificationsP: [],
-        userId: null,
-        txtReply: null,
-        showInput2: false,
-        outerVisible: false,
-        innerVisible: false,
-        showModal: false
-      }
-    },
-    methods: {
-      showModal2(){
-        this.outerVisible = true
-        this.updateNotif()
-      },
-      replyNot(notification) {
-        this.showInput2 = true
-        this.txtReply = 'Reply to ' + notification.name
-        this.notification_id_parent = notification.notification_id
-        this.layer_id = notification.layer_id
-      },
-      clearNot(notification) {
-        const vm = this
-        Api().delete('/api/notification/?notification_id=' + notification.notification_id,
-        ).then(function (response) {
-          //console.log(response)
-          vm.updateNotif()
-        })
-      },
-      reportNot(notification) {
-        this.showInput2 = true
-        this.txtReply = 'Denunciation to ' + notification.name
-        this.is_denunciation = true
-        this.notification_id_parent = notification.notification_id
-        this.layer_id = notification.layer_id
-      },
-      clearMsg() {
-        this.txtReply = null
-        this.notification_id_parent = null
-        this.layer_id = null
-        this.keyword_id = null
-        this.is_denunciation = false
-        this.showInput2 = false
-      },
-      // handleClick(tab, event) {
-      //   console.log(tab, event);
-      // },
-      addNotif() {
-        const vm = this
-        let msg = ''
-        if (vm.txtNotif !== null) {
-          let notification = {
-            'properties': {
-              'notification_id': -1,
-              'is_denunciation': this.is_denunciation,
-              'keyword_id': this.keyword_id,
-              'notification_id_parent': this.notification_id_parent,
-              'layer_id': this.layer_id,
-              'description': this.txtNotif,
-            },
-            'type': 'Notification'
-          }
+import Api from '@/middleware/ApiVGI'
+import iconPerson from '@/views/assets/images/icon_person.png'
 
-          Api().post('/api/notification/create', notification).then(function (response) {
-            vm.$message.success("The notification was added with success!")
-            vm.clearMsg()
-            vm.txtNotif = null
-            vm.updateNotif()
-          }, function (cause) {
-            msg = cause.toString()
-            console.log(cause.response)
-            vm.$message.error(msg)
-          })
-        } else {
-          msg = "it is necessary to have some text."
-          vm.$message.error(msg)
-        }
-      },
-      orderNotification(x) {
-        const vm = this
-        setTimeout(_ => {
-          vm.notifG.sort(function (a, b) {
-            return new Date(b.date) - new Date(a.date)
-          })
-        }, x);
-      },
-      updateNotif(){
-        const vm = this
-        this.notifG = []
-
-        Api().get('/api/notification/?notification_id='+vm.notification_id).then(function (notification) {
-          Api().get('/api/user/?user_id=' + notification.data.features[0].properties.user_id_creator).then(function (user) {
-            vm.notification = {
-                'description': notification.data.features[0].properties.description,
-                'name': user.data.features[0].properties.name,
-                'created_at': notification.data.features[0].properties.created_at,
-                'type': 'myLayer',
-                'notification_id': notification.data.features[0].properties.notification_id,
-                'user_id_creator': notification.data.features[0].properties.user_id_creator,
-                'is_denunciation': notification.data.features[0].properties.is_denunciation,
-                'keyword_id': notification.data.features[0].properties.keyword_id,
-                'layer_id': notification.data.features[0].properties.layer_id,
-                'notification_id_parent': notification.data.features[0].properties.notification_id_parent,
-              }
-            vm.orderNotification(50)
-          })
-        })
-
-        Api().get('/api/notification/?notification_id_parent='+vm.notification_id).then(function (notifications) {
-          notifications.data.features.forEach(notification => {
-            Api().get('/api/user/?user_id=' + notification.properties.user_id_creator).then(function (user) {
-              vm.notifG.push(
-                {
-                  'description': notification.properties.description,
-                  'name': user.data.features[0].properties.name,
-                  'date': notification.properties.created_at,
-                  'type': 'myLayer',
-                  'notification_id': notification.properties.notification_id,
-                  'user_id_creator': notification.properties.user_id_creator,
-                  'is_denunciation': notification.properties.is_denunciation,
-                  'keyword_id': notification.properties.keyword_id,
-                  'layer_id': notification.properties.layer_id,
-                  'notification_id_parent': notification.properties.notification_id_parent
-                })
-              vm.orderNotification(50)
-            })
-          })
-        })
-      }
-    },
-    mounted() {
-      this.updateNotif()
-      // $('#myModal').on('shown.bs.modal', function () {
-      //   $('#myInput').trigger('focus')
-      // })
+export default {
+  name: "ModalNotification",
+  props: ['master_notification'],
+  components: {
+    "modal": {
+      template: '#modal-template'
     }
-  }
+  },
+  computed: {
+    ...mapState('auth', ['user']),
+    ...mapState('map', ['boxNotifications'])
+  },
+  data () {
+    return {
+      comments: [],
+      description: null,
+      outerVisible: false,
+      is_denunciation: false,
+      // txtReply: null,
+      // layer_id: null,
+      // showInput2: false
+    }
+  },
+  mounted() {
+    this.updateComments()
+  },
+  methods: {
+    showModalNotification() {
+      this.outerVisible = true
+      this.updateComments()
+    },
+    closeModalNotification() {
+      this.outerVisible = false
+    },
+    addComment() {
+      if (this.description === null || this.description === '') {
+        this.$message.error("Comment cannot be empty!")
+        return
+      }
 
+      this.__openFullLoading()
+
+      let comment = {
+        'properties': {
+          'id': -1,
+          'description': this.description,
+          'is_denunciation': this.is_denunciation,
+          'notification_id_parent': this.master_notification.id,
+          'keyword_id': null,
+          'layer_id': null
+        },
+        'type': 'Notification'
+      }
+
+      Api().post('/api/notification/create', comment).then(() => {
+        this.description = null
+        // this.cleanMessage()
+        this.$message.success("The comment was added successfully!")
+      }).catch(cause => {
+        this.__error_message(cause)
+      }).finally(() => {
+        this.updateComments()
+        this.__closeFullLoading()
+      })
+    },
+    updateComments() {
+      this.comments = []
+
+      // get all comments (i.e. sub notifications) from master notification
+      Api().get(
+        '/api/notification/?notification_id_parent=' + this.master_notification.id
+      ).then(response => {
+        this.comments = response.data.features.map(c => {
+          // fix user picture to each comment
+          c.properties.user_picture = c.properties.user_picture === '' ?
+                                        iconPerson : c.properties.user_picture
+
+          return c.properties
+        })
+      }).catch(cause => {
+        this.__error_message(cause)
+      })
+    },
+    canUserRemoveIt(comment) {
+      return this.user !== null &&
+              (this.user.user_id === comment.user_id_creator || this.user.is_the_admin)
+    },
+    removeComment(comment) {
+      this.__openFullLoading()
+
+      Api().delete(
+        '/api/notification/?notification_id=' + comment.id,
+      ).then(() => {
+        this.$message.success("The comment was removed successfully!")
+      }).catch(cause => {
+        this.__error_message(cause)
+      }).finally(() => {
+        this.updateComments()
+        this.__closeFullLoading()
+      })
+    },
+    __openFullLoading(){
+      this.loading = this.$loading({
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      })
+    },
+    __closeFullLoading(){
+      if (this.loading !== null)
+        this.loading.close()
+    },
+    __error_message(cause){
+      this.$message.error(cause.response.data.toString())
+    },
+    // cleanMessage() {
+    //   // this.txtReply = null
+    //   this.is_denunciation = false
+    //   // this.layer_id = null
+    //   // this.showInput2 = false
+    // },
+    // replyNotification(notification) {
+    //   this.showInput2 = true
+    //   this.txtReply = 'Reply to ' + notification.user_name
+    //   this.layer_id = notification.layer_id
+    // },
+    // reportNotification(notification) {
+    //   this.showInput2 = true
+    //   this.txtReply = 'Denunciation to ' + notification.user_name
+    //   this.is_denunciation = true
+    //   this.layer_id = notification.layer_id
+    // },
+  }
+}
 </script>
 
 <style lang="sass" scoped>
-  .el-button
+  .el-button:focus, .el-button:hover
     color: #ff6107
+    border-color: #ff6107
+    background-color: #fff
 
   .notification-box2
     margin: 10px
     margin-right: 60px
-    background: rgba(183,200,255,0.69)
+    background: rgba(183, 200, 255, 0.70)
     padding: 20px
     border-radius: 20px
     left: 50px
@@ -259,27 +262,25 @@
       display: block
 
   .photo
-    display: inline-block
     width: 40px
-    padding: 10px
     text-align: center
-    color: #FFF
     border-radius: 50%
-    background: #666
 
   .credentials
     display: inline-block
     margin: 0 0 0 10px
+
     .author
       font-weight: 600
       margin-top: 5px !important
       font-size: 1.1em
+
     .date
-      color: #666
+      color: #595959
       font-size: 0.9em
+
     p
       margin: 0 0 5px 0 !important
-
 
   .comments
     width: 100%
@@ -301,5 +302,4 @@
 
   .msgType
     display: flex
-
 </style>
