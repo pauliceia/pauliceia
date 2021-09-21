f28e01a681a5c5f31d5d372c02a0a4e5124ba227
Rodrigo Mariano <rodmariano13@gmail.com>
2021-09-21 16:13:56 -0300

    feat: add admin button to remove layers on the dashboard; add callbacks to promise to remove layers; add loading component

--- src/views/pages/dashboard/home.vue
+++ src/views/pages/dashboard/home.vue
@@ -13,25 +13,27 @@
         </div>
       </div>
 
-      <!-- layers lists -->
+      <!-- list of layers -->
       <div class="col-sm-4">
 
-        <!-- my layers list -->
+        <!-- list of my layers -->
         <div class="card ">
           <div class="card-body">
             <h6 class="mb-0">{{ $t('dashboard.home.myLayers') }}</h6>
             <br>
             <div class="card-text">
               <div class="row" v-for="layer in myLayers" v-bind:key="layer.id">
-                <div class="col-sm-7">{{ layer.name }}</div>
-                <div class="col-sm-5">
-                  <button type="button" class="btn btn-outline-danger btn-sm add2"
-                      @click="deleteLayer(layer.id)">
-                    <md-icon>clear</md-icon>
-                  </button>
-                  <button type="button" class="btn btn-outline-dark btn-sm add"
-                      @click="editLayer(layer.id)">
+                <div class="col-sm-8">{{ layer.name }}</div>
+                <div class="col-sm-2">
+                  <button type="button" @click="editLayer(layer.id)"
+                          class="btn btn-outline-dark btn-sm add">
                     <md-icon>create</md-icon>
+                  </button>
+                </div>
+                <div class="col-sm-2">
+                  <button type="button" @click="deleteLayer(layer.id)"
+                          class="btn btn-outline-danger btn-sm add2">
+                    <md-icon>clear</md-icon>
                   </button>
                 </div>
                 <hr>
@@ -40,7 +42,7 @@
           </div>
         </div>
 
-        <!-- shared layers list -->
+        <!-- list of shared layers -->
         <div class="card " style="margin-top: 20px">
           <div class="card-body">
             <h6 class="mb-0">{{ $t('dashboard.home.sharedLayers') }}</h6>
@@ -49,13 +51,17 @@
               <div class="row" v-for="layer in sharedLayers" v-bind:key="layer.id">
                 <div class="col-sm-8">{{ layer.name }}</div>
                 <div class="col-sm-2">
-                  <button type="button" class="btn btn-outline-dark btn-sm add"
-                      @click="editLayer(layer.id)">
+                  <button type="button" @click="editLayer(layer.id)"
+                          class="btn btn-outline-dark btn-sm add">
                     <md-icon>create</md-icon>
                   </button>
                 </div>
                 <div class="col-sm-2">
-                  <!--<button type="button" class="btn btn-outline-danger btn-sm add" @click="deleteLayer(layer.id)"><md-icon>clear</md-icon></button>-->
+                  <!-- just admin user can delete all layers -->
+                  <button type="button" v-if="user.is_the_admin" @click="deleteLayer(layer.id)"
+                          class="btn btn-outline-danger btn-sm add2">
+                    <md-icon>clear</md-icon>
+                  </button>
                 </div>
                 <hr>
               </div>
@@ -70,84 +76,119 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+import { mapState } from 'vuex'
 
-  import Api from '@/middleware/ApiVGI'
-  import DashLayout from '@/views/layouts/dashboard'
-  import Notifications from '@/views/components/dashboard/Notifications'
+import Api from '@/middleware/ApiVGI'
+import DashLayout from '@/views/layouts/dashboard'
+import Notifications from '@/views/components/dashboard/Notifications'
 
-  export default {
-    data () {
-      return {
-        myLayers: [],
-        sharedLayers: []
-      }
-    },
-    mounted() {
-      this.updateLayers()
-      this.orderLayers(500)
-    },
-    computed: {
-      ...mapState('auth', ['user']),
-      ...mapState('map', ['boxNotifications']),
-    },
-    methods: {
-      deleteLayer(id){
-        Api().delete('/api/layer/' + id).then(() => {
-          this.updateLayers()
-        })
-      },
-      editLayer(id){
-        this.$router.push({name: 'EditLayer', params: {layer_id: id}})
-      },
-      updateLayers(){
-        this.sharedLayers = []
-        this.myLayers = []
-
-        Api().get('/api/user_layer/?user_id=' + this.user.id).then((response) => {
-          response.data.features.filter(e => {
-
-            Api().get('/api/layer/?id=' + e.properties.layer_id).then((response2) => {
-              if (e.properties.is_the_creator)
-                this.myLayers.push(response2.data.features[0].properties)
-              else
-                this.sharedLayers.push(response2.data.features[0].properties)
-
-              this.orderLayers(10)
-            })
-          })
-        })
-      },
-      orderLayers(x){
-        setTimeout(_ => {
-          this.myLayers.sort((a, b) => {
-            if (a.name.toLowerCase() > b.name.toLowerCase())
-              return 1;
-
-            if (a.name.toLowerCase() < b.name.toLowerCase())
-              return -1;
-
-            // a must be equal to b
-            return 0;
-          })
-          this.sharedLayers.sort((a, b) => {
-            if (a.name.toLowerCase() > b.name.toLowerCase())
-              return 1;
-
-            if (a.name.toLowerCase() < b.name.toLowerCase())
-              return -1;
-
-            // a must be equal to b
-            return 0;
-          })
-        }, x);
-      }
-    },
-    components: {
-      "p-dash-layout": DashLayout,
-      'p-notifications': Notifications
+export default {
+  components: {
+    "p-dash-layout": DashLayout,
+    'p-notifications': Notifications
+  },
+  computed: {
+    ...mapState('auth', ['user']),
+    ...mapState('map', ['boxNotifications'])
+  },
+  data () {
+    return {
+      myLayers: [],
+      sharedLayers: []
     }
+  },
+  mounted() {
+    this.updateLayers()
+  },
+  methods: {
+    editLayer(id){
+      this.$router.push({name: 'EditLayer', params: {layer_id: id}})
+    },
+    deleteLayer(layer_id){
+      this.__openFullLoading()
+
+      Api().delete('/api/layer/' + layer_id).then(() => {
+        this.$message.success("A camada foi excluída com sucesso!")
+      }).catch(cause => {
+        this.__showErrorMessages(cause)
+      }).finally(() => {
+        this.updateLayers()
+        this.__closeFullLoading()
+      })
+    },
+    updateLayers(){
+      this.sharedLayers = []
+      this.myLayers = []
+
+      Api().get('/api/user_layer/?user_id=' + this.user.id).then(response => {
+        response.data.features.filter(user_layer => {
+
+          Api().get('/api/layer/?id=' + user_layer.properties.layer_id).then(response2 => {
+            if (user_layer.properties.is_the_creator)
+              this.myLayers.push(response2.data.features[0].properties)
+            else
+              this.sharedLayers.push(response2.data.features[0].properties)
+          })
+        })
+      })
+
+      // this.__orderLayers(1000)
+    },
+    __errorMessage(msg){
+      this.$message.error({
+        message: msg,
+        center: true,
+        duration: 10000,
+        showClose: true,
+      })
+    },
+    __showErrorMessages(cause) {
+      if ('data' in cause.response)
+        this.__errorMessage(cause.response.data.toString())
+      else
+        this.__errorMessage(cause.toString())
+
+      if (cause.response.status >= 500)
+        this.__errorMessage("Problem when creating a resource. Please, contact the administrator.")
+    },
+    __openFullLoading(){
+      this.loading = this.$loading({
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      })
+    },
+    __closeFullLoading(){
+      if (this.loading !== null)
+        this.loading.close()
+    },
+    // __orderLayers(x){
+    //   setTimeout(_ => {
+    //     this.myLayers.sort((a, b) => {
+    //       if (a.name.toLowerCase() > b.name.toLowerCase())
+    //         return 1
+
+    //       if (a.name.toLowerCase() < b.name.toLowerCase())
+    //         return -1
+
+    //       // a must be equal to b
+    //       return 0
+    //     })
+    //     this.sharedLayers.sort((a, b) => {
+    //       if (a.name.toLowerCase() > b.name.toLowerCase())
+    //         return 1
+
+    //       if (a.name.toLowerCase() < b.name.toLowerCase())
+    //         return -1
+
+    //       // a must be equal to b
+    //       return 0
+    //     })
+    //   }, x)
+    // }
   }
+}
 </script>
 
 <style lang="sass" scoped>
