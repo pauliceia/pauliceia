409e36984c90c6bd0e2b1b121ea25cdc107932b5
VitorVan <vitor.vanjura@hotmail.com>
2024-01-09 12:53:48 -0300

    added acceptance tests directory and config

---
+++ src/tests/features/support/credential.rb
@@ -0,0 +1,9 @@
+# coding utf-8
+
+CREDENCIAL = {
+  email: 'pauliceiateste@gmail.com',
+  password: 'Testepauliceia12345',
+  name: 'Teste Pauliceia',
+  username: 'pauliceiateste',
+  refABNT: 'Autor: Sobrenome, Iniciais do(s) Nome(s). Título do Livro. Edição (se não for a primeira). Local de publicação: Editora, Ano de publicação. Número total de páginas (se aplicável).'
+}
