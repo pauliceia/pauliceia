1ddc71f43f8405b429edffadccb796c4e1034270
Lucas Panta de Moura <88425511+LumenRuby@users.noreply.github.com>
2024-01-09 16:56:57 -0300

    Make all links clickable, correct CC link on contact page

--- src/views/pages/contact.vue
+++ src/views/pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-      <div class="row justify-content-md-center row-style" itemscope itemtype="https://schema.org/Organization">
+  <div>
+     <div class="row justify-content-md-center row-style" itemscope itemtype="https://schema.org/Organization">
         <div class="col-sm-4 column-style">
           <img class="picture" src="@/views/assets/images/untitled-4.png">
           <p itemprop="name">PAULICEIA 2.0:</p>
@@ -9,42 +9,51 @@
           <p>{{ $t('contact.contact') }}</p>
           <p class="picture2 orange-style"><md-icon>mail</md-icon></p>
           <p class="orange-style" itemprop="email">pauliceia_support[at]googlegroups.com</p>
-          <p><img class="picture2" style="width: 25px; " src="@/views/assets/images/facebook.png">/grupohimaco</p>
+          <p><a :href="$t('https://facebook.com/grupohimaco')" target="_blank"><img class="picture2" style="width: 25px;" src="@/views/assets/images/facebook.png">/grupohimaco</a></p>
+          <p><a :href="$t('https://github.com/pauliceia')" target="_blank"><img class="picture2" style="width: 25px;" src="@/views/assets/images/github.png">/pauliceia</a></p>
+        </div>
 
-        </div>
-        <div class="col-sm-4 column-style">
-          <p class="orange-style"><b>{{ $t('contact.links') }}</b></p>
-          <br>
-          <p class="orange-style"><b>{{ $t('contact.institutional') }}</b></p>
-          <p itemscope itemtype="https://schema.org/Organization">
-            <b itemprop="name">{{ $t('contact.envolvidos.arquivo_publico_sp') }}:</b> <span itemprop="url">www.arquivoestado.sp.gov.br</span></p>
-          <p itemscope itemtype="https://schema.org/Organization">
-            <b itemprop="name">{{ $t('contact.envolvidos.emory_university') }}:</b> <span itemprop="url">www.emory.edu</span></p>
-          <p itemscope itemtype="https://schema.org/Organization">
-            <b itemprop="name">{{ $t('contact.envolvidos.fapesp') }}:</b> <span itemprop="url">www.fapesp.br</span></p>
-          <p itemscope itemtype="https://schema.org/Organization">
-            <b itemprop="name">{{ $t('contact.envolvidos.himaco') }}:</b> <span itemprop="url">www.unifesp.br/himaco</span></p>
-          <p itemscope itemtype="https://schema.org/Organization">
-            <b itemprop="name">{{ $t('contact.envolvidos.inpe') }}:</b> <span itemprop="url">www.inpe.br</span></p>
-          <p itemscope itemtype="https://schema.org/Organization">
-            <b itemprop="name">{{ $t('contact.envolvidos.unifesp') }}:</b> <span itemprop="url">www.unifesp.br</span></p>
-          <br>
-          <p class="orange-style"><b>{{ $t('contact.platforms') }}</b></p>
-          <p><b>Creative Commons:</b> https//creativecommons.org/</p>
-          <p><b>gvSIG:</b> www.gvsig.com/pt</p>
-          <p><b>TerraBrasilis:</b> http://terrabrasilis.dpi.inpe.br</p>
-          <br>
-          <p class="orange-style"><b>{{ $t('contact.base') }}</b></p>
-          <p><b>Open Street Map:</b> www.openstreetmap.org</p>
-        </div>
+      <div class="col-sm-4 column-style">
+        <p class="orange-style"><b>{{ $t('contact.links') }}</b></p>
+        <br>
+        <p class="orange-style"><b>{{ $t('contact.institutional') }}:</b></p>
+        <p itemscope itemtype="https://schema.org/Organization">
+          <b itemprop="name"><a :href="$t('https://www.arquivoestado.sp.gov.br')" target="_blank">{{ $t('contact.envolvidos.arquivo_publico_sp') }}</a></b>
+        </p>   
+        <p itemscope itemtype="https://schema.org/Organization">
+            <b itemprop="name"><a :href="$t('https://www.emory.edu/home/')" target="_blank">{{ $t('contact.envolvidos.emory_university') }}</a></b>
+        </p>
+        <p itemscope itemtype="https://schema.org/Organization">
+          <b itemprop="name"><a :href="$t('https://fapesp.br/')" target="_blank">{{ $t('contact.envolvidos.fapesp') }}</a></b>
+        </p>
+        <p itemscope itemtype="https://schema.org/Organization">
+          <b itemprop="name"><a :href="$t('http://www2.unifesp.br/himaco/')" target="_blank">{{ $t('contact.envolvidos.himaco') }}</a></b>
+        </p>
+        <p itemscope itemtype="https://schema.org/Organization">
+          <b itemprop="name"><a :href="$t('https://www.gov.br/inpe/')" target="_blank">{{ $t('contact.envolvidos.inpe') }}</a></b>
+        </p>
+        <p itemscope itemtype="https://schema.org/Organization">
+          <b itemprop="name"><a :href="$t('https://www.unifesp.br/')" target="_blank">{{ $t('contact.envolvidos.unifesp') }}</a></b>
+        </p>
+
+        <br>
+        <p class="orange-style"><b>{{ $t('contact.platforms') }}:</b></p>
+        <p><b><a :href="$t('https://creativecommons.org/')" target="_blank">Creative Commons</a></b></p>
+        <p><b><a :href="$t('http://www.gvsig.com')" target="_blank">gvSIG</a></b></p>
+        <p><b><a :href="$t('http://terrabrasilis.dpi.inpe.br')" target="_blank">TerraBrasilis</a></b></p>
+
+        <br>
+        <p class="orange-style"><b>{{ $t('contact.base') }}:</b></p>
+        <p><b><a :href="$t('https://www.openstreetmap.org')" target="_blank">Open Street Map</a></b></p>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "contact"
-    }
+export default {
+  name: "contact"
+}
 </script>
 
 <style lang="sass" scoped>
@@ -64,5 +73,5 @@
 
   .orange-style
     color: #ff6107
-
 </style>
+
