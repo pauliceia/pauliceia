38ac6de1a367e5b053a183a34825c38c0dc35163
VitorVan <vitor.vanjura@hotmail.com>
2024-01-09 13:48:53 -0300

    added acceptance tests for temporal data search in add layer modal

---
+++ src/tests/features/bdd/search_temporal_data_add_layer.feature
@@ -0,0 +1,8 @@
+# language: pt
+
+Funcionalidade: Pesquisar informações temporais de camadas no menu de adicionar e remover camadas
+
+Cenário: Pesquisar informações temporais de camadas no menu de adicionar e remover camadas
+Dado que eu esteja na tela de adicionar e remover camadas
+E eu pesquiso por "31/12/1975"
+Então todos os resultados visiveis devem conter "31/12/1975"
---
+++ src/tests/features/step_definitions/search_temporal_data_add_layer_steps.rb
@@ -0,0 +1,9 @@
+Dado('eu pesquiso por {string}') do |termo_pesquisa|
+  find('input[placeholder="Pesquise por tema, camada, autor ou dados temporais:"]').set(termo_pesquisa)
+end
+
+Então('todos os resultados visiveis devem conter {string}') do |termo_pesquisa|
+  elements = all('.box-layer-info')
+  all_elements_contain_search = elements.all? { |element| element.text.include?(termo_pesquisa) }
+  expect(all_elements_contain_search).to be true
+end
