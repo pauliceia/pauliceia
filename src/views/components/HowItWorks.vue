194637cbd5777f9c47b2589084edc66fd136a5d7
Pedro Taciano <taciano@usp.br>
2024-01-14 20:57:33 -0300

    feat: componentize home

---
+++ src/views/components/HowItWorks.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="about" class="how-wrapper">
+      <h3>
+        {{ $t('home.how') }}
+      </h3>
+
+      <div class="how-container">
+        <div class="how-item">
+          <img src="@/views/assets/images/home/add.svg" alt="add">
+          <h5>{{ $t('home.howTitle1') }}</h5>
+          <p>{{ $t('home.howText1') }}</p>
+        </div>
+
+        <div class="how-item">
+          <img src="@/views/assets/images/home/acumulate.svg" alt="visualize">
+          <h5>{{ $t('home.howTitle2') }}</h5>
+          <p>{{ $t('home.howText2') }}</p>
+        </div>
+
+        <div class="how-item">
+          <img src="@/views/assets/images/home/world.svg" alt="compare">
+          <h5>{{ $t('home.howTitle3') }}</h5>
+          <p>{{ $t('home.howText3') }}</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass" scoped>
+  .how-wrapper
+    padding: 100px 5%
+    color: #c6c6c6
+    font-family: 'Archivo', sans-serif     
+    .how-container
+      display: flex
+      justify-content: space-between
+      align-items: center
+      
+      .how-item
+        display: flex
+        flex-direction: column
+        align-items: flex-start
+        gap: 12px
+        width: 444px   
+        img
+          margin-top: 80px
+          margin-bottom: 60px      
+        h5
+          font-size: 24px
+          font-weight: 600
+          line-height: 1.5     
+        p
+          font-size: 18px
+          line-height: 1.5
+          font-weight: 300
+          opacity: 0.8
+</style>
--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -26,42 +26,22 @@
       </div>
     </div>
 
-    <div id="about" class="how-wrapper">
-      <h3>
-        {{ $t('home.how') }}
-      </h3>
+    <HowItWorks />
 
-      <div class="how-container">
-        <div class="how-item">
-          <img src="@/views/assets/images/home/add.svg" alt="add">
-          <h5>{{ $t('home.howTitle1') }}</h5>
-          <p>{{ $t('home.howText1') }}</p>
-        </div>
-
-        <div class="how-item">
-          <img src="@/views/assets/images/home/acumulate.svg" alt="visualize">
-          <h5>{{ $t('home.howTitle2') }}</h5>
-          <p>{{ $t('home.howText2') }}</p>
-        </div>
-
-        <div class="how-item">
-          <img src="@/views/assets/images/home/world.svg" alt="compare">
-          <h5>{{ $t('home.howTitle3') }}</h5>
-          <p>{{ $t('home.howText3') }}</p>
-        </div>
-      </div>
-    </div>
+    <FAQ />
 
   </div>
 </template>
 
 <script>
-import HowToCite from '@/views/components/HowToCite.vue'
+import HowItWorks from '@/views/components/HowItWorks.vue'
+import FAQ from '@/views/components/FAQ.vue'
 
 export default {
   name: "home",
   components: {
-    HowToCite
+    HowItWorks,
+    FAQ
   }
 }
 </script>
@@ -128,38 +108,5 @@ export default {
             img
               width: 42px
               aspect-ratio: 1/1
-    
-    .how-wrapper
-      padding: 100px 5%
-      color: #c6c6c6
-      font-family: 'Archivo', sans-serif
-
-      .how-container
-        display: flex
-        justify-content: space-between
-        align-items: center
-        
-        .how-item
-          display: flex
-          flex-direction: column
-          align-items: flex-start
-          gap: 12px
-          width: 444px
-
-          img
-            margin-top: 80px
-            margin-bottom: 60px 
-
-          h5
-            font-size: 24px
-            font-weight: 600
-            line-height: 1.5
-
-          p
-            font-size: 18px
-            line-height: 1.5
-            font-weight: 300
-            opacity: 0.8
-
-        
+          
 </style>
