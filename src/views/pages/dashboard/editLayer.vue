6a4837897a08df9f25fdad3083d3411f4db70033
Bruno Baffini <bruno.baffini@quintoandar.com.br>
2024-01-14 12:10:12 -0300

    Adding end date limit

--- src/views/pages/dashboard/editLayer.vue
+++ src/views/pages/dashboard/editLayer.vue
@@ -294,6 +294,11 @@
           return;
         }
 
+        if(this.endDate > Date.now()){
+          this._msgError("A data final não pode ser maior que a data atual!")
+          return;
+        }
+
         let temporalColumns = {
           'properties': {
             'f_table_name': this.tableName,
