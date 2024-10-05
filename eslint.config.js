85af633988ab02ee6bf36943b491bdf9d8c6a5d0
Emmanuel <emanusdias@gmail.com>
2024-10-05 14:37:54 -0300

    remove empty templates + rule exception

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,6 @@ module.exports = [
 
     rules: {
       "vue/multi-word-component-names": "off",
-      "vue/valid-template-root": "off",
       "no-unused-vars": [
         "error",
         {
--- src/views/pages/validSocial.vue
+++ src/views/pages/validSocial.vue
@@ -1,5 +1,3 @@
-<template></template>
-
 <script>
 import User from "@/middleware/User";
 
