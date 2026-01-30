390e2701426938852bc2e497e9c323ee686e0d72
raphael augusto <rapharaos@hotmail.com>
2026-01-29 21:59:56 -0300

    adjust method ol createdEmpty

--- src/views/components/map/Geocoding.vue
+++ src/views/components/map/Geocoding.vue
@@ -540,23 +540,18 @@ export default {
           });
           this.loading.close();
         } else {
-          // let search = this.inputSearch.replace(/( )+/g, " ");
+          let search = this.inputSearch.replace(/( )+/g, " ");        
+          let text =
+            "Não encontramos pontos necessarios para a geolocalização nesse logradouro no ano buscado (" +
+            search +
+            ")";
 
-          // console.log(error);
-          // console.log(search);
-
-          // console.log('aoooooba');
-          // let text =
-          //   "Não encontramos pontos necessarios para a geolocalização nesse logradouro no ano buscado (" +
-          //   search +
-          //   ")";
-
-          // this.$alert(text, "Erro", {
-          //   dangerouslyUseHTMLString: true,
-          //   confirmButtonText: "OK",
-          //   type: "error",
-          // });
-          // this.loading.close();
+          this.$alert(text, "Erro", {
+            dangerouslyUseHTMLString: true,
+            confirmButtonText: "OK",
+            type: "error",
+          });
+          this.loading.close();
         }
       }
     },
